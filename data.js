147fa3b2456601d769959e91d0e901db6b6be6f5
vencoslav1357 <155269304+vencoslav1357@users.noreply.github.com>
2025-07-05 20:13:18 +0200

    upgrade

---
+++ data.js
@@ -0,0 +1,72 @@
+// Data pro aktuality
+const newsData = [
+    {
+        date: {
+            day: 15,
+            month: 10,
+            year: 2023
+        },
+        title: "Podzimní výcvik",
+        mainImage: "logo.png",
+        preview: "První podzimní výcvik proběhl na cvičišti...", // Krátký popis
+        fullText: "Zde bude dlouhý text s kompletním popisem události...", // Plný text
+        gallery: ["vycvik_2023/foto1.png", "vycvik_2023/foto2.png", "vycvik_2023/foto1.png"] // Další fotky
+    },
+
+    {
+        date: {
+            day: 15,
+            month: 10,
+            year: 2023
+        },
+        title: "Podzimní výcvik",
+        mainImage: "logo.png",
+        preview: "První podzimní výcvik proběhl na cvičišti...", // Krátký popis
+        fullText: "Zde bude dlouhý text s kompletním popisem události...", // Plný text
+        gallery: ["vycvik_2023/foto1.png", "vycvik_2023/foto2.png", "vycvik_2023/foto1.png"] // Další fotky
+    },
+
+    {
+        date: {
+            day: 15,
+            month: 10,
+            year: 2023
+        },
+        title: "Podzimní výcvik",
+        mainImage: "logo.png",
+        preview: "První podzimní výcvik proběhl na cvičišti...", // Krátký popis
+        fullText: "Zde bude dlouhý text s kompletním popisem události...", // Plný text
+        gallery: ["vycvik_2023/foto1.png", "vycvik_2023/foto2.png", "vycvik_2023/foto1.png"] // Další fotky
+    },
+    // ...další zprávy
+];
+
+// Data pro akce
+const eventsData = [
+    {
+        date: "2023-11-12",
+        title: "Prezentace pro rodiče",
+        text: "Zveme všechny rodiče na prezentaci činnosti kroužku"
+    },
+    {
+        date: "2023-11-12",
+        title: "Prezentace pro rodiče",
+        text: "Zveme všechny rodiče na prezentaci činnosti kroužku"
+    },
+    {
+        date: "2023-11-11",
+        title: "Prezentace pro rodiče",
+        text: "Zveme všechny rodiče na prezentaci činnosti kroužku"
+    },
+    {
+        date: "2023-11-12",
+        title: "Prezentace pro rodiče",
+        text: "Zveme všechny rodiče na prezentaci činnosti kroužku"
+    },
+    {
+        date: "2025-12-2",
+        title: "priklad",
+        text: "ahoj"
+    },
+    // Přidat další akce zde
+];

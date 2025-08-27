c2401464c95c591e6ccfd65ad24b77cdbfeb8527
pavelcekal <pavelcekal148@gmail.com>
2025-08-27 20:11:17 +0200

    Soutez Porinn

--- data.js
+++ data.js
@@ -13,7 +13,21 @@ const newsData = [
         gallery: ["vycvik_2023/foto1.png", "vycvik_2023/sdh_examplephoto.jpeg"] // Další fotky
     },
 
-
+{
+    date: {
+        day: 1,
+        month: 7,
+        year: 2025
+    },
+    title: "Soutěž v Poříně",
+    mainImage: "images/vycvik/Soutez Porin.heic",
+    preview: "", // Krátký popis
+    fullText: "Soutěž v Poříně proběhla fantasticky. Bylo hodně pití a jídla i radosti.", // Plný text
+    gallery: [
+        "images/vycvik/IMG_3723.heic",
+        "images/vycvik/IMG_3724.heic"
+    ]
+},
     
     {
         date: {

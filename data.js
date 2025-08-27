5818fbdd05db67088ffb5c415fa5bad9558bde27
vencoslav1357 <gekoncicek@gmail.com>
2025-08-27 20:21:10 +0100

    Update data.js

--- data.js
+++ data.js
@@ -1,17 +1,17 @@
 // Data pro aktuality
 const newsData = [
-    {
-        date: {
-            day: 15,
-            month: 10,
-            year: 2023
-        },
-        title: "Podzimní výcvik",
-        mainImage: "logo.png",
-        preview: "První podzimní výcvik proběhl na cvičišti...", // Krátký popis
-        fullText: "Zde bude dlouhý text s kompletním popisem události...", // Plný text
-        gallery: ["vycvik_2023/foto1.png", "vycvik_2023/sdh_examplephoto.jpeg"] // Další fotky
-    },
+    // {
+//     date: {
+//         day: 15,
+//         month: 10,
+//         year: 2023
+//     },
+//     title: "Podzimní výcvik",
+//     mainImage: "logo.png",
+//     preview: "První podzimní výcvik proběhl na cvičišti...", // Krátký popis
+//     fullText: "Zde bude dlouhý text s kompletním popisem události...", // Plný text
+//     gallery: ["vycvik_2023/foto1.png", "vycvik_2023/sdh_examplephoto.jpeg"] // Další fotky
+// },
 
 {
     date: {
@@ -23,7 +23,7 @@ const newsData = [
     mainImage: "Pavel/soutez-porin/Soutez3.jpg",
     preview: "", // Krátký popis
     fullText: "Soutěž v Poříně proběhla skvěle. Bylo hodně pití a jídla i zábavy.", // Plný text
-    gallery: ["Pavel/soutez-porin/Soutez2.jpg",]
+    gallery: ["Pavel/soutez-porin/Soutez2.jpg"]
 },
     
     {
@@ -47,15 +47,15 @@ const newsData = [
 
 // Data pro akce
 const eventsData = [
-    {
-        date: {
-            day: 30,
-            month: 8,
-            year: 2025
-        },
-        title: "Pouť",
-        text: "Tradiční pouť pořádaná v obci Pořín. (testovací text)",
-    },
+// {
+//     date: {
+//         day: 30,
+//         month: 8,
+//         year: 2025
+//     },
+//     title: "Pouť",
+//     text: "Tradiční pouť pořádaná v obci Pořín. (testovací text)",
+// },
 
    
     {

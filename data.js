ec17eb3e743322959af1adfbc271bdef89fa6ecc
terkasvackova <terkasva@gmail.com>
2025-10-04 16:47:47 +0200

    Update data.js

--- data.js
+++ data.js
@@ -20,10 +20,10 @@ const newsData = [
         year: 2025
     },
     title: "Podzimní plamen v Ústrašicích"
-    mainImage: "",
+    mainImage: "pes.jpeg",
     preview: "", // Krátký popis
     fullText: "Týmy byly moc šikovné a bojovné. Nejlépe se umístila přípravka, která vybojovala krásné 1. místo. Mladší se umístili na 9. místě a starší na 18. místě.", // Plný text
-    gallery: [""]
+    gallery: ["terka/pes.jpeg"]
 },
 
 
